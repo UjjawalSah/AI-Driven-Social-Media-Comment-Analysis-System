3b2978bd8b9c623ab0b01efb846fb6ae2e45bf24
👨‍💻 Ujjawal Kumar <116669610+UjjawalSah@users.noreply.github.com>
2025-01-31 13:59:25 +0530

    Delete utils directory

--- utils/preprocess.py
+++
@@ -1,7 +0,0 @@
-import re
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(r"http\S+|www\S+|https\S+", "", text)
-    text = re.sub(r"[^a-zA-Z\s]", "", text)
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
